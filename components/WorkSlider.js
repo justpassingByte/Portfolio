96267f8a98fe5b94091dad8faf607abc7c56e9cd
ngusitink <nguyenhuuthang20012003@gmail.com>
2026-04-28 11:04:48 +0700

    update asset

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -20,7 +20,7 @@ const projects = [
   },
   {
     title: 'TFT Grimoire',
-    paths: ['/tftgrimoire.PNG', '/tftgrimoire-2.png'],
+    paths: ['/tierlist.jpeg', '/tierlistmaker.jpeg', '/compbuilder.jpeg', '/admincomptierlist.jpeg', '/admincomptierlistbuilder.jpeg'],
     link: 'https://grimoire.testictour.com',
     description: 'Player identity & profile system',
   },
